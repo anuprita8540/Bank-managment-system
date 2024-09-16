2094069cba616a1e27cf4a1e127eb9374a459817
anuprita8540 <borudeanuprita@gmail.com>
2024-09-17 01:02:04 +0530

    login frame created

---
+++ Login.java
@@ -0,0 +1,17 @@
+package bank.managment.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame{
+
+    Login(){
+        setSize(800,480);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+        
+    }
+}
